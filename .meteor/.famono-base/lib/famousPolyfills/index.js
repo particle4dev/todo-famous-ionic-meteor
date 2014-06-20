35f6f2e665cd8d28495fe3e708ffe6e786326474
Particle4Dev <steve@p4d-Vostro-270.(none)>
2014-06-20 22:35:12 +0700

    remove files

--- .meteor/.famono-base/lib/famousPolyfills/index.js
+++
@@ -1,5 +0,0 @@
-define('famousPolyfills/index', ["famousPolyfills/classList","famousPolyfills/functionPrototypeBind","famousPolyfills/requestAnimationFrame"], function(require, exports, module) {
-require('famousPolyfills/classList');
-require('famousPolyfills/functionPrototypeBind');
-require('famousPolyfills/requestAnimationFrame');
-});
